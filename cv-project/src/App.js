3333b3621f6c3f1b921898aec6f52ac225747c36
cechua <chuacelc08@gmail.com>
2023-05-05 21:45:30 +0800

    Add reset and generation of sample cv details

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -7,30 +7,26 @@ import PreviewContainer from "components/previewComponent/PreviewContainer";
 import "react-datepicker/dist/react-datepicker.css";
 function App() {
   const [previewValues, setPreviewValues] = useState({
-    firstName: "FirstName",
-    lastName: "LastName",
-    contact: "+639121233444",
-    email: "sample@email.com",
-    address: "123 Random Street Random City",
+    firstName: "",
+    lastName: "",
+    contact: "",
+    email: "",
+    address: "",
     experience: [
       {
-        company: "Fake Company 1",
-        position: "Front End Developer",
-        startDate: "05/04/2023",
-        endDate: "05/04/2027",
+        company: "",
+        position: "",
+        startDate: "",
+        endDate: "",
         jobDetails: [""],
       },
     ],
     educationBackground: [
       {
-        school: "Fake School",
-        startYear: "05/04/2021",
-        endYear: "01/02/2024",
-        schoolDetails: [
-          "Major in Information Technology",
-          "Scholarship recipient",
-          "Dean lister S.Y. 2022-2023 1st Semester",
-        ],
+        school: "",
+        startYear: "",
+        endYear: "",
+        schoolDetails: ["", "", ""],
       },
     ],
     others: [""],
@@ -43,7 +39,10 @@ function App() {
           previewValues={previewValues}
           setPreviewValues={setPreviewValues}
         />
-        <PreviewContainer previewValues={previewValues} />
+        <PreviewContainer
+          previewValues={previewValues}
+          setPreviewValues={setPreviewValues}
+        />
       </div>
       <Footer />
     </div>
--- cv-project/src/components/previewComponent/PreviewContainer.js
+++ cv-project/src/components/previewComponent/PreviewContainer.js
@@ -2,10 +2,93 @@ import React, { useRef } from "react";
 import PreviewBox from "./PreviewBox";
 import { useReactToPrint } from "react-to-print";
 
-const PreviewContainer = ({ previewValues }) => {
+const PreviewContainer = ({ previewValues, setPreviewValues }) => {
   const generatePDF = useReactToPrint({
     content: () => componentRef.current,
   });
+  const resetDetails = () => {
+    setPreviewValues({
+      firstName: "",
+      lastName: "",
+      contact: "",
+      email: "",
+      address: "",
+      experience: [
+        {
+          company: "",
+          position: "",
+          startDate: "",
+          endDate: "",
+          jobDetails: [""],
+        },
+      ],
+      educationBackground: [
+        {
+          school: "",
+          startYear: "",
+          endYear: "",
+          schoolDetails: ["", "", ""],
+        },
+      ],
+      others: [""],
+    });
+  };
+  const generateFakeDetails = () => {
+    setPreviewValues({
+      firstName: "James",
+      lastName: "Doe",
+      contact: "1234567890",
+      email: "sample@email.com",
+      address: "123 Random Street Random City",
+      experience: [
+        {
+          company: "Fake Company 1",
+          position: "Front End Developer",
+          startDate: "05/04/2023",
+          endDate: "",
+          jobDetails: [
+            "Develop and maintains web application",
+            "Uses ReactJS with TypeScript, GraphQL, MaterialUI",
+            "Collaborate with teammates with the help of JIRA and GIT",
+            "Manages to finish tickets on a timely manner",
+          ],
+        },
+        {
+          company: "Fake Company 2",
+          position: "Back End Developer",
+          startDate: "05/04/2020",
+          endDate: "05/04/2023",
+          jobDetails: [
+            "Develop and maintain API Services",
+            "Use NodeJS and MongoDB",
+            "Collaborate with teammates with the help of JIRA and GIT",
+            "Manages to finish tickets on a timely manner",
+          ],
+        },
+      ],
+      educationBackground: [
+        {
+          school: "Fake School",
+          startYear: "05/04/2016",
+          endYear: "01/02/2020",
+          schoolDetails: [
+            "Bachlor of Sciense in Information Technology: Major in Web Development",
+            "Scholarship recipient",
+            "Dean lister S.Y. 2022-2023 1st Semester",
+          ],
+        },
+      ],
+      others: [
+        "Programming languages knowldge: Javascript, React, SQL, NodeJS,GraphQL, TypeScript, HTML & CSS",
+        "Application/Software used: Visual Studio Code, Postman",
+        "Languages spoken: English, Spanish, Chinese, Japanese",
+        "Team player",
+        "Able to collaborate with teammates efficiently",
+        "Willing to be trained for new technologies",
+        "Eager to learn new tech stacks",
+      ],
+    });
+  };
   const componentRef = useRef();
   return (
     <div className="preview-container">
@@ -16,6 +99,12 @@ const PreviewContainer = ({ previewValues }) => {
       />
       <button className="button-add" onClick={generatePDF}>
         Generate PDF
+      </button>
+      <button className="button-add" onClick={generateFakeDetails}>
+        Generate Sample CV
+      </button>
+      <button className="button-add" onClick={resetDetails}>
+        Clear
       </button>
     </div>
   );
